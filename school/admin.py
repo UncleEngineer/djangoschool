1f1bbca0892f9c1686fc895333be1236838fcdca
Uncle Engineer <loong.wissawakorn@gmail.com>
2020-04-19 21:53:33 +0700

    Add files via upload

--- school/admin.py
+++ school/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import ExamScore
+from .models import ExamScore, AllStudent, Profile, DocumentUpload
+
 # Register your models here.
 
 admin.site.register(ExamScore)
+
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ['student_id','level','student_name','student_tel']
+	list_filter = ['level']
+	list_editable = ['student_tel']
+
+
+admin.site.register(AllStudent, StudentAdmin)
+admin.site.register(Profile)
+admin.site.register(DocumentUpload)
